001c22ddf088e745a800aff3a90857ed4bd238fa
itwill <oorosemaryoo99@gmail.com>
2025-08-08 14:08:35 +0900

    ..

--- 10.객체간의관계(상속)/src/AcademyGangsa.java
+++ 10.객체간의관계(상속)/src/AcademyGangsa.java
@@ -11,5 +11,18 @@ public class AcademyGangsa extends AcademyMember {
 		this.subject = subject;
 	}
 	
+	public void gansaPrint() {
+		this.print();
+		System.out.println(this.subject);
+	}
+	
+	
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
 
 }
