70f23f6a98c06d76ac1d8cc674bdb62ad22cb3f0
itwill <oorosemaryoo99@gmail.com>
2025-08-08 14:11:34 +0900

    ..

--- 10.객체간의관계(상속)/src/AcademySteff.java
+++ 10.객체간의관계(상속)/src/AcademySteff.java
@@ -1,3 +1,24 @@
 public class AcademySteff extends AcademyMember {
+	
+	
+	public String depart; //부서
+	public AcademySteff() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public AcademySteff(int no, String name, String depart) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.depart = depart;
+		
+	}
+	
+	public void staffPrint() {
+		this.print();
+		System.out.println(this.depart);
+	}
+	
+	
 
 }
